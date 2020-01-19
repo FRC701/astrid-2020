ea10c5a1d45981db70d01472e5998e11765b6079
Corina <corinar@vandenrobotics.com>
2020-01-19 10:46:08 -0800

    Made Conveyor Ball Algorithm

---
+++ src/main/include/commands/BallEnter.h
@@ -0,0 +1,23 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <frc2/command/CommandHelper.h>
+#include <frc2/command/InstantCommand.h>
+#include <subsystems/Conveyer.h>
+
+class BallEnter
+    : public frc2::CommandHelper<frc2::InstantCommand,
+                                 BallEnter> {
+ public:
+  BallEnter(Conveyer* conveyer);
+
+  void Initialize() override;
+private:
+  Conveyer* mConveyer;
+};
