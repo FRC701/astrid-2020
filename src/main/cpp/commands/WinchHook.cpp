01a1b6f279c784226be50bb837fcb3fcaefa4e8f
Julie Tolentino <juliet@vandenrobotics.com>
2020-01-25 14:47:01 -0800

    Commands for Telescope and Winch
    Rising the telescope and making the winch hook on

---
+++ src/main/cpp/commands/WinchHook.cpp
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "commands/WinchHook.h"
+
+WinchHook::WinchHook(std::function<double()> percentoutput,
+                      Winch& winch)
+: mWinchMotor(percentoutput), mWinch(winch)
+{
+  // Use addRequirements() here to declare subsystem dependencies.
+  AddRequirements(&mWinch);
+}
+
+// Called when the command is initially scheduled.
+void WinchHook::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void WinchHook::Execute() 
+{
+  mWinch.WinchHook(mWinchMotor());
+}
+
+// Called once the command ends or is interrupted.
+void WinchHook::End(bool interrupted) {}
+
+// Returns true when the command should end.
+bool WinchHook::IsFinished() { return false; }
