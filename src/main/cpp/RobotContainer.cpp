9502e7b17e72b389b1c1c0801dfb4871841cd114
Nicolas Schallock <nics@vandenrobotics.com>
2020-01-24 16:36:28 -0800

    Buttons for Intake

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -4,17 +4,22 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-#include <frc2/command/button/Trigger.h>
 #include "RobotContainer.h"
-#include "frc/smartdashboard/SmartDashboard.h"
-#include "commands/TankDrive.h"
+
+#include <frc2/command/button/Trigger.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+
+#include "commands/IntakeOn.h"
 #include "commands/SetConveyor.h"
+#include "commands/TankDrive.h"
 
 RobotContainer::RobotContainer()
 {
   using JoystickHand = frc::GenericHID::JoystickHand;
 
   // Initialize all of your commands and subsystems here
+
+  // Configure the button bindings
   mChassis.SetDefaultCommand
   (
     TankDrive
@@ -24,6 +29,17 @@ RobotContainer::RobotContainer()
       [this] { return driver.GetY(JoystickHand::kRightHand); }
     )
   );
+
+  frc::SmartDashboard::PutData("Intake 10 percent", new IntakeOn(0.1, mIntake));
+  frc::SmartDashboard::PutData("Intake 20 percent", new IntakeOn(0.2, mIntake));
+  frc::SmartDashboard::PutData("Intake 30 percent", new IntakeOn(0.3, mIntake));
+  frc::SmartDashboard::PutData("Intake 40 percent", new IntakeOn(0.4, mIntake));
+  frc::SmartDashboard::PutData("Intake 50 percent", new IntakeOn(0.5, mIntake));
+  frc::SmartDashboard::PutData("Intake 60 percent", new IntakeOn(0.6, mIntake));
+  frc::SmartDashboard::PutData("Intake 70 percent", new IntakeOn(0.7, mIntake));
+  frc::SmartDashboard::PutData("Intake 80 percent", new IntakeOn(0.8, mIntake));
+  frc::SmartDashboard::PutData("Intake 90 percent", new IntakeOn(0.9, mIntake));
+  frc::SmartDashboard::PutData("Intake 100 percent", new IntakeOn(1.0, mIntake));
 
   mConveyor.SetDefaultCommand(SetConveyor(mConveyor, 0.2));
 
