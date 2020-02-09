1d57a1dab2c47e0521d216009502a12a0c4ab5a4
Nicolas Schallock <nics@vandenrobotics.com>
2020-02-09 12:29:25 -0800

    made grouping system for intaking

---
+++ src/main/include/commands/IntakeCommand.h
@@ -0,0 +1,23 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <frc2/command/CommandHelper.h>
+#include <frc2/command/ParallelRaceGroup.h>
+#include "subsystems/Conveyor.h"
+#include "subsystems/Intake.h"
+
+class IntakeCommand
+    : public frc2::CommandHelper<frc2::ParallelRaceGroup,
+                                 IntakeCommand> {
+ public:
+  IntakeCommand(Intake& intake, Conveyor& conveyor);
+ private:
+  Intake& mIntake;
+  Conveyor& mConveyor;
+};
