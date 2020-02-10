8683934fe566f36fc46d12582000455cf20abb7a
Nicolas Schallock <nics@vandenrobotics.com>
2020-02-09 17:00:59 -0800

    Shooter: EnableShoot and Friends (wip)
    Still need to tune the coordination between the shooter and the conveyor. Implement IsShooting() that calls the shooter.

--- src/main/cpp/commands/ShootBalls.cpp
+++ src/main/cpp/commands/ShootBalls.cpp
@@ -5,28 +5,14 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-#include "commands/EnableShoot.h"
-#include "commands/Aim.h"
-#include "commands/LongHood.h"
-#include "commands/SetConveyor.h"
-#include "commands/Shoot.h"
 #include "commands/ShootBalls.h"
-#include "commands/StowHood.h"
+#include "commands/Shoot.h"
+#include "commands/SetConveyor.h"
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.
 // For more information, see:
 // https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
-EnableShoot::EnableShoot(Chassis& chassis, Conveyor& conveyor, Shooter& shooter)
-: mChassis(chassis), mConveyor(conveyor), mShooter(shooter)
-{
+ShootBalls::ShootBalls(Shooter& shooter, Conveyor& conveyor, double shooterRPM) {
   // Add your commands here, e.g.
-  // AddCommands(FooCommand(), BarCommand());
-  //
-  // What is this magic number? What is the calculation?
-  AddCommands(
-    LongHood(shooter), 
-    /* Aim(mChassis), */
-    ShootBalls(shooter, conveyor, 2000), // 2000RPM for testing
-    StowHood(shooter)
-  );
+  AddCommands(Shoot(shooter, shooterRPM), SetConveyor(conveyor, 0.3));
 }
